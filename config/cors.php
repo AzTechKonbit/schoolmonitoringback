56730b984cafe8fc22608bc30d61210271bfbb29
Dupy007 <jfd.social@outlook.fr>
2026-06-08 20:40:24 +0200

    :zap: Add cors

---
+++ config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'paths' => [
+        '*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:5174',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
